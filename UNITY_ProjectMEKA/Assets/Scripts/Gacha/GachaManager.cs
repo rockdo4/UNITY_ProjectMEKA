d3be9ae83f0b478fabd6ae83294919bcc8ba06c3
이남석 <2namsuk2@gmail.com>
2023-11-20 16:48:53 +0900

    테스트 뽑기 버튼 및 결과 확인 추가

--- UNITY_ProjectMEKA/Assets/Scripts/Gacha/GachaManager.cs
+++ UNITY_ProjectMEKA/Assets/Scripts/Gacha/GachaManager.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public struct GachaTestClass
+public struct GachaTestCharacter
 {
     public int id;
     public string name;
     public int weight;
 
-    public GachaTestClass(string name, int weight)
+    public GachaTestCharacter(string name, int weight)
     {
         this.name = name;
         this.weight = weight;
@@ -18,20 +21,27 @@ public struct GachaTestClass
 
 public class GachaManager : MonoBehaviour
 {
-    private GachaSystem<GachaTestClass> testPicker;
+    public GameObject resultPanel;
+    public Image imagePrefab;
+    public TextMeshProUGUI resultText;
+    public int[] count;
+
+    private GachaSystem<GachaTestCharacter> testPicker;
 
     private void Awake()
     {
-		testPicker = new GachaSystem<GachaTestClass>();
+		testPicker = new GachaSystem<GachaTestCharacter>();
 	}
 
     private void Start()
     {
-        for (int i = 1; i < 11; i++) 
+        for (int i = 1; i <= 20; i++) 
         {
-            var testClass = new GachaTestClass(i.ToString(), i * 10);
+            var testClass = new GachaTestCharacter(i.ToString(), i * 10);
             testPicker.Add(testClass, testClass.weight);
         }
+
+        count = new int[20];
     }
 
     private void Update()
@@ -41,16 +51,58 @@ public class GachaManager : MonoBehaviour
 
     public void Gacha1()
     {
+        var arr = resultPanel.GetComponentsInChildren<Image>();
+        foreach(var it in arr)
+        {
+			if (it.gameObject == resultPanel) continue;
+			Destroy(it.gameObject);
+        }
+
 		var item = testPicker.GetItem();
 		Debug.Log($"이름 : {item.name}, 가중치 : {item.weight}");
+
+        var itemImage = Instantiate(imagePrefab);
+        itemImage.transform.SetParent(resultPanel.transform);
+		itemImage.GetComponentInChildren<TextMeshProUGUI>().SetText(item.name);
+        itemImage.color = Color.red * (30 - int.Parse(item.name)) / 30;
+		count[int.Parse(item.name) - 1]++;
+		SetText();
 	}
 
     public void Gacha10()
     {
+		var arr = resultPanel.GetComponentsInChildren<Image>();
+		foreach (var it in arr)
+		{
+            if (it.gameObject == resultPanel) continue;
+			Destroy(it.gameObject);
+		}
+
 		var item = testPicker.GetItem(10);
+
 		foreach (var it in item)
 		{
 			Debug.Log($"이름 : {it.name}, 가중치 : {it.weight}");
+			var itemImage = Instantiate(imagePrefab);
+			itemImage.transform.SetParent(resultPanel.transform);
+            itemImage.GetComponentInChildren<TextMeshProUGUI>().SetText(it.name);
+			itemImage.color = Color.red * (30 - int.Parse(it.name)) / 30;
+
+            count[int.Parse(it.name) - 1]++;
 		}
+
+        SetText();
 	}
+
+    public void SetText()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i=0; i<count.Length; i++)
+        {
+            stringBuilder.Append($"[{i} : {count[i]}] / ");
+        }
+
+        resultText.SetText(stringBuilder.ToString());
+    }
 }
