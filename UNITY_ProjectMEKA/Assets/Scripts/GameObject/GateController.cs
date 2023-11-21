339a5bcdd0ee3a2176352f969b92c71b519aa70c
langbo22@naver.com <langbo22@naver.com>
2023-11-21 19:44:15 +0900

    적 유닛 웨이브 적용 스폰 구현

--- UNITY_ProjectMEKA/Assets/Scripts/GameObject/GateController.cs
+++ UNITY_ProjectMEKA/Assets/Scripts/GameObject/GateController.cs
@@ -20,12 +20,20 @@ public struct WaveInfo
 public class GateController : MonoBehaviour
 {
     // 이동 관련
+    [HideInInspector]
     public Defines.GateType gateType;
-    public GameObject[] waypoints;
+    private GameObject[] waypoints;
 
     // 몬스터 스폰 관련
     [SerializeField]
     public List<WaveInfo> waveInfos;
+
+    private int currentWave = 0;
+    private int currentEnemyType = 0;
+    private int currentEnemyCount = 1;
+    private float spawnTimer = 0f;
+    private float waveTimer = 0f;
+    private bool once = false;
 
     private void Awake()
     {
@@ -45,14 +53,76 @@ public class GateController : MonoBehaviour
         }
     }
 
-    // 몬스터 스폰 함수
     private void Start()
     {
-        
     }
 
     private void Update()
     {
-        
+        if (currentWave >= waveInfos.Count)
+            return;
+
+        if (waveTimer > 0)
+        {
+            waveTimer -= Time.deltaTime;
+            return;
+        }
+
+        SpawnEnemies();
+    }
+
+    // 몬스터 스폰 함수
+    private void SpawnEnemies()
+    {
+        var enemyInfo = waveInfos[currentWave].enemySpawnInfos[currentEnemyType];
+        if(!once)
+        {
+            Debug.Log(enemyInfo.type.ToString());
+            var enemyGo = ObjectPoolManager.instance.GetGo(enemyInfo.type.ToString());
+            enemyGo.transform.position = transform.position;
+
+            once = true;
+        }
+
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer < waveInfos[currentWave].enemySpawnInfos[currentEnemyType].interval)
+            return;
+
+        var enemy = ObjectPoolManager.instance.GetGo(enemyInfo.type.ToString());
+        enemy.transform.position = transform.position;
+        currentEnemyCount++;
+        spawnTimer = 0f;
+
+        if(currentEnemyCount >= enemyInfo.count)
+        {
+            currentEnemyCount = 0;
+            currentEnemyType++;
+        }
+
+        if (currentEnemyType >= waveInfos[currentWave].enemySpawnInfos.Count)
+        {
+            currentEnemyType = 0;
+            waveTimer = waveInfos[currentWave].waveInterval;
+            currentWave++;
+        }
+
+
+
+        //// 기존 코드
+        //foreach (var wave in waveInfos)
+        //{
+        //    foreach(var waveEnemyInfo in wave.enemySpawnInfos)
+        //    {
+        //        for(int i = 0; i < waveEnemyInfo.count; ++i)
+        //        {
+        //            var enemy = ObjectPoolManager.instance.GetGo(waveEnemyInfo.type.ToString());
+        //            enemy.transform.position = transform.position;
+        //            // enemy 웨이포인트 세팅
+
+        //            // interval 후 다음 몬스터 생성
+        //        }
+        //    }
+        //    // spawnInterval 후 다음 웨이브 몬스터 생성
+        //}
     }
 }
--- UNITY_ProjectMEKA/Assets/Scripts/Utils/Defines.cs
+++ UNITY_ProjectMEKA/Assets/Scripts/Utils/Defines.cs
@@ -21,7 +21,7 @@ public class Defines
     public enum EnemyType
     {
         None,
-        MonsterTest,
+        Monster,
         Enemy1,
         Enemy2,
         Enemy3,
