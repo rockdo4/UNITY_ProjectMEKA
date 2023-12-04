af793d3c5bb2ec46a2b48957df29a1b704eb7c85
이남석 <2namsuk2@gmail.com>
2023-12-04 11:34:52 +0900

    레벨업 입력 방식 살짝 수정, 머지 전 커밋

--- UNITY_ProjectMEKA/Assets/Scripts/Utils/ArrangeJoystick.cs
+++ UNITY_ProjectMEKA/Assets/Scripts/Utils/ArrangeJoystick.cs
@@ -1,9 +1,7 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
-using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 using static PlayerController;
 
