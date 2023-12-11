176259301dec320f247eb1a23256e1fae8dea17d
김민지 <langbo22@naver.com>
2023-12-11 12:04:03 +0900

    - 오브젝트풀매니저에서 안 쓰는 몬스터 삭제 - 스킬 버튼 누르면 idle state로 전환

--- UNITY_ProjectMEKA/Assets/Scripts/Tables/CharacterTable.cs
+++ UNITY_ProjectMEKA/Assets/Scripts/Tables/CharacterTable.cs
@@ -1,7 +1,6 @@
-using CsvHelper.Configuration;
 using CsvHelper;
+using CsvHelper.Configuration;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
