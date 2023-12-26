53da70e4eccce8addd44b2d493b33cde4ecf0ff7
이남석 <2namsuk2@gmail.com>
2023-12-26 21:55:11 +0900

    - 대화 데이터 테이블 임시 주석 처리

--- UNITY_ProjectMEKA/Assets/Scripts/Tables/AffectionCommunicationTable.cs
+++ UNITY_ProjectMEKA/Assets/Scripts/Tables/AffectionCommunicationTable.cs
@@ -6,65 +6,77 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using UnityEngine;
-using Unity.VisualScripting;
 
-public class AffectionCommunicationTable : DataTable
+public class CommuinicationDictionary
 {
-	protected Dictionary<int, List<CommunicationData>> communicationDictList = new Dictionary<int, List<CommunicationData>>();
-	public AffectionCommunicationTable()
-	{
-		path = "Table/AffectionCommunicationTable";
-		Load();
-	}
-
-	public override void Load()
-	{
-		var csvData = Resources.Load<TextAsset>(path);
-
-		TextReader reader = new StringReader(csvData.text);
-
-		var csvConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture);
-		csvConfiguration.HasHeaderRecord = true;
-
-		var csv = new CsvReader(reader, csvConfiguration);
-
-		try
-		{
-			var records = csv.GetRecords<CommunicationData>();
-
-			foreach (var record in records)
-			{
-				CommunicationData temp = record;
-				
-				if(!communicationDictList.ContainsKey(temp.CharacterID))
-				{
-					var list = new List<CommunicationData>();
-
-					communicationDictList.Add(temp.CharacterID, list);
-					communicationDictList[temp.CharacterID].Add(temp);
-				}
-				else
-				{
-					communicationDictList[temp.CharacterID].Add(temp);
-				}
-
-				//Debug.Log(temp.Script);
-			}
-		}
-		catch (Exception ex)
-		{
-			Debug.Log(ex.Message);
-			Debug.LogError("csv 로드 에러");
-		}
-	}
-
-	public List<CommunicationData> GetAffectionData(int CharacterID)
-	{
-		if (communicationDictList.ContainsKey(CharacterID))
-		{
-			Debug.LogWarning("캐릭터가 보유한 대화 없음");
-			return null;
-		}
-		return communicationDictList[CharacterID];
-	}
+	//캐릭터 대화 종류 ID, 그 ID에 해당하는 리스트
+	public Dictionary<int, List<CommunicationData>> idCommunicationList;
 }
+
+//public class AffectionCommunicationTable : DataTable
+//{
+//	//캐릭터 <ID, 캐릭터 대화 내용>
+//	protected Dictionary<int, CommuinicationDictionary> characterCommunicationDict = new Dictionary<int, CommuinicationDictionary>();
+//	public AffectionCommunicationTable()
+//	{
+//		path = "Table/AffectionCommunicationTable";
+//		Load();
+//	}
+
+//	public override void Load()
+//	{
+//		var csvData = Resources.Load<TextAsset>(path);
+
+//		TextReader reader = new StringReader(csvData.text);
+
+//		var csvConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture);
+//		csvConfiguration.HasHeaderRecord = true;
+
+//		var csv = new CsvReader(reader, csvConfiguration);
+
+//		try
+//		{
+//			var records = csv.GetRecords<CommunicationData>();
+
+//			foreach (var record in records)
+//			{
+//				CommunicationData temp = record;
+
+//				if (!characterCommunicationDict.ContainsKey(temp.CharacterID))
+//				{
+//					var list = new CommuinicationDictionary();
+//					characterCommunicationDict.Add(temp.CharacterID, list);
+//				}
+
+//				if (!characterCommunicationDict[temp.CharacterID].Exists(x => x.key == temp.ID))
+//				{
+//					var data = new CommuinicationDictionary();
+//					data.key = temp.ID;
+//					data.dataList = new List<CommunicationData>();
+//					data.dataList.Add(temp);
+//				}
+//				else
+//				{
+//					characterCommunicationDict[temp.CharacterID].Find(x => x.key == temp.ID).dataList.Add(temp);
+//				}
+//				Debug.Log((temp.ID, temp.Script));
+//				//Debug.Log(temp.Script);
+//			}
+//		}
+//		catch (Exception ex)
+//		{
+//			Debug.Log(ex.Message);
+//			Debug.LogError("csv 로드 에러");
+//		}
+//	}
+
+//	public List<CommuinicationDictionary> GetAffectionData(int CharacterID)
+//	{
+//		if (characterCommunicationDict.ContainsKey(CharacterID))
+//		{
+//			Debug.LogWarning("캐릭터가 보유한 대화 없음");
+//			return null;
+//		}
+//		return characterCommunicationDict[CharacterID];
+//	}
+//}
