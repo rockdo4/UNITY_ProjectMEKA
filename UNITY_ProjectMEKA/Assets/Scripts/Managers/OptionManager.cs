25622dd929305b72789f97ca6ba7a7a3061d2ffe
김민지 <langbo22@naver.com>
2023-12-26 15:18:56 +0900

    <OptionManager> - 나가기 버튼 구현

--- UNITY_ProjectMEKA/Assets/Scripts/Managers/OptionManager.cs
+++ UNITY_ProjectMEKA/Assets/Scripts/Managers/OptionManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using static Defines;
 
 public class OptionManager : MonoBehaviour
 {
     protected GameObject optionPanel;
     protected Button optionButton;
+    private IngameStageUIManager ingameUIManager;
 
     public Sprite[] soundButtonSprites = new Sprite[2];
     public Sprite[] pauseButtonSprites = new Sprite[2];
@@ -22,13 +25,25 @@ public class OptionManager : MonoBehaviour
 
     private void Awake()
     {
+        ingameUIManager = GameObject.FindGameObjectWithTag(Tags.characterInfoUIManager).GetComponent<IngameStageUIManager>();
         buttonLayerName = LayerMask.LayerToName(gameObject.layer);
         optionButton = GetComponent<Button>();
         optionPanel = transform.GetChild(0).gameObject;
         optionButton.onClick.AddListener(OpenAndCloseOptionPanel);
-        soundButton.onClick.AddListener(() => { ChangeButtonSprite(soundButton, soundButtonSprites); });
-        pauseButton.onClick.AddListener(() => { ChangeButtonSprite(pauseButton, pauseButtonSprites); });
-        speedButton.onClick.AddListener(() => { ChangeButtonSprite(speedButton, speedButtonSprites); });
+
+        exitButton.onClick.AddListener(() =>
+        {
+            ingameUIManager.CloseScene();
+        });
+        soundButton.onClick.AddListener(() => 
+        { ChangeButtonSprite(soundButton, soundButtonSprites); 
+        });
+        pauseButton.onClick.AddListener(() => 
+        { ChangeButtonSprite(pauseButton, pauseButtonSprites);
+        });
+        speedButton.onClick.AddListener(() => 
+        { ChangeButtonSprite(speedButton, speedButtonSprites); 
+        });
     }
 
     private void Update()
@@ -66,7 +81,6 @@ public class OptionManager : MonoBehaviour
 
     public void ChangeButtonSprite(Button button, Sprite[] spritePool)
     {
-        //var currentSprite = button.GetComponent<Image>().sprite;
         for(int i = 0; i < spritePool.Length; ++i)
         {
             if (button.GetComponent<Image>().sprite == spritePool[i])
@@ -83,24 +97,4 @@ public class OptionManager : MonoBehaviour
             }
         }
     }
-
-    //public void OnClickPauseButton()
-    //{
-    //    var pauseButtonSprite = pauseButton.GetComponent<Image>().sprite;
-    //    for (int i = 0; i < pauseButtonSprites.Length; ++i)
-    //    {
-    //        if (pauseButtonSprite == pauseButtonSprites[i])
-    //        {
-    //            if (i >= soundButtonSprites.Length - 1)
-    //            {
-    //                pauseButtonSprite = pauseButtonSprites[0];
-    //            }
-    //            else
-    //            {
-    //                pauseButtonSprite = pauseButtonSprites[i + 1];
-    //            }
-    //            break;
-    //        }
-    //    }
-    //}
 }
