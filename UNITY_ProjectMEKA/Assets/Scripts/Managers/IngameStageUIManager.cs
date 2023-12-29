812dc3b006fc7b973293560f2d1797123db6dedf
이남석 <2namsuk2@gmail.com>
2023-12-29 09:18:51 +0900

    - UI_Merge_2 씬으로 메인씬 변경 및 테스트

--- UNITY_ProjectMEKA/Assets/Scripts/Managers/IngameStageUIManager.cs
+++ UNITY_ProjectMEKA/Assets/Scripts/Managers/IngameStageUIManager.cs
@@ -294,7 +294,7 @@ public class IngameStageUIManager : MonoBehaviour
         Time.timeScale = 1f;
         StageDataManager.Instance.toStageChoicePanel = true;
         //SceneManager.LoadScene("MainScene");
-        SceneManager.LoadScene("MainScene_UI_Merge_Latest");
+        SceneManager.LoadScene("MainScene_UI_Merge_2");
     }
 
     public void ChangeCharacterInfo()
